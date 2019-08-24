b484eabc37dd9845b2bfd6df9815f96cc80b2395
somesh3168 <somesh3168@gmail.com>
2019-08-24 23:24:25 +0530

    first python file added

---
+++ youtube.py
@@ -0,0 +1,15 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name('test.json', scope)
+
+client = gspread.authorize(credentials)
+
+wks = client.open('testSheet').sheet1
+
+print(wks.get_all_records())
+print(wks.range)
+
+#https://docs.google.com/spreadsheets/d/1A2C7qxAwT9xopYMWOqyUhBOHdIWRRHxak6-wmegAX_k/edit#gid=0
