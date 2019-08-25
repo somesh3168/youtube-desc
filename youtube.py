4ac1bfce372664d4d05129952cf837fe7158562f
somesh3168 <somesh3168@gmail.com>
2019-08-25 20:39:48 +0530

    working on django now

--- youtube.py
+++ youtube.py
@@ -5,43 +5,17 @@ data = pd.read_csv('test.csv', delimiter = ',',
 print("Enter Youtube Link")
 input_link = input()
 x = len(data.link)
-print('Index no.',x)
+
 for i in range(1, x):
     if input_link == data.link[i]:
+        print('Index no.',i)
         print(data.title[i])
         print('Desc\n')
-        desc = str(data.subject[i]) + '\n'+ str(data.chap[i])+ '\n'+ str(data.hin[i])+ '\n'+ str(data.mb[i])
+        desc = str(data.subject[i]) + '\n'+ str(data.chap[i])+ '\n'+ str(data.hin[i])+ '\n'+ str(data.mb[i])+ '\n'+ str(data.tags[i])+ '\n'+ str(data.prev[i])+ '\n'+ str(data.next[i])
         print(desc)
 
 
-# print(filename[6])
 
-
-
-
-
-
-
-
-
-
-
-
-# with open(filename, 'r') as csvfile:
-#     csvreader = csv.reader(csvfile)
-
-#     fields = csvreader.next()
-
-#     for rows in csvreader:
-#         rows.append(row)
-
-#     print("Total no. of rows: %d"%(csvreader.line_num))    
-
-#commit only 3
 #1 - https://docs.google.com/spreadsheets/d/1A2C7qxAwT9xopYMWOqyUhBOHdIWRRHxak6-wmegAX_k/edit#gid=0
 #2 - https://docs.google.com/spreadsheet/ccc?key=0ArM5yzzCw9IZdEdLWlpHT1FCcUpYQ2RjWmZYWmNwbXc&output=csv
 
-# --- import the csv file to python -- make upload file option on the page
-# --- use pandas aas pd to scale
-# --- rearrange the data into TITLE, DESC... parts
-# --- 
